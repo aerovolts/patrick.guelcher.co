c906d4cf2be9130bce4e7a6948cd5894b61bfae3
Patrick Guelcher <aeroblitz@pwpg.ca>
2016-05-28 22:10:15 -0400

    Complete re-write of most of the site
    Updated to latest versions of MDL, FontAwesome
    Utilized portfolio template from MDL
    Portfolio re-work in progress

--- main.py
+++ main.py
@@ -4,24 +4,31 @@ app = Flask(__name__)
 
 author = 'Patrick Guelcher'
 year = '2016'
+credits = 'Template from Google Material Design; Modified by Myself.'
 
 @app.route('/')
 def home():
-    title = 'Home | Patrick Guelcher'
+    title = 'Patrick Guelcher'
     description = 'The home of Patrick Guelcher on the internet.'
-    return render_template('pages/index.html', author=author, year=year, title=title, description=description)
+    return render_template('pages/index.html', author=author, year=year, credits=credits, title=title, description=description)
 
-@app.route('/resume')
+@app.route('/about')
 def about():
-    title = 'Resume | Patrick Guelcher'
-    description = 'A short background and list of skills about Patrick Guelcher.'
-    return render_template('pages/resume.html', author=author, year=year, title=title, description=description)
+    title = 'Patrick Guelcher'
+    description = 'About the author of this portfolio website Patrick Guelcher.'
+    return render_template('pages/about.html', author=author, year=year, credits=credits, title=title, description=description)
+
+@app.route('/contact')
+def contact():
+    title = 'Contact Me | Patrick Guelcher'
+    description = 'A listing of methods for contacting the website author Patrick Guelcher.'
+    return render_template('pages/contact.html', author=author, year=year, credits=credits, title=title, description=description)
 
 @app.route('/portfolio')
 def portfolio():
     title = 'Portfolio | Patrick Guelcher'
     description = 'A portfolio of cartographic products produced by Patrick Guelcher in various software packages.'
-    return render_template('pages/portfolio.html', author=author, year=year, title=title, description=description)
+    return render_template('pages/portfolio.html', author=author, year=year, credits=credits, title=title, description=description)
 
 if __name__ == '__main__':
     app.run()
