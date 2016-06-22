c33ccf45d35013722681ec7041b3fcb5ce9a9fc7
Patrick Guelcher <aeroblitz@pwpg.ca>
2016-06-22 16:37:42 -0400

    Design scheme overhaul

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-author = 'Patrick Guelcher'
-year = '2016'
-credits = 'Template from Google Material Design; Modified by Myself.'
+app.config.from_object(__name__)
 
 @app.route('/')
 def home():
-    title = 'Patrick Guelcher'
-    description = 'The home of Patrick Guelcher on the internet.'
-    return render_template('pages/index.html', author=author, year=year, credits=credits, title=title, description=description)
+    description = 'The home of Patrick Guelcher on the internet. This personal website contains a blog and portfolio.'
+    return render_template('pages/index.html', description=description)
 
 @app.route('/about')
 def about():
-    title = 'Patrick Guelcher'
-    description = 'About the author of this portfolio website Patrick Guelcher.'
-    return render_template('pages/about.html', author=author, year=year, credits=credits, title=title, description=description)
+    description = 'About the author of this website and portfolio Patrick Guelcher.'
+    return render_template('pages/about.html', description=description)
 
 @app.route('/contact')
 def contact():
-    title = 'Contact Me | Patrick Guelcher'
     description = 'A listing of methods for contacting the website author Patrick Guelcher.'
-    return render_template('pages/contact.html', author=author, year=year, credits=credits, title=title, description=description)
+    return render_template('pages/contact.html', description=description)
 
 @app.route('/portfolio')
 def portfolio():
-    title = 'Portfolio | Patrick Guelcher'
     description = 'A portfolio of cartographic products produced by Patrick Guelcher in various software packages.'
-    return render_template('pages/portfolio.html', author=author, year=year, credits=credits, title=title, description=description)
+    return render_template('pages/portfolio.html', description=description)
 
 def main():
     app.run()
